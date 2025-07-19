64418686c05a9dcec9f5ed381b69c0ff5da58597
Adi <adieitc@yahoo.com>
2025-07-19 16:10:01 -0400

    Fixups

--- src/components/ShipmentMap.tsx
+++ src/components/ShipmentMap.tsx
@@ -7,7 +7,7 @@ import L from 'leaflet';
 import React, { useEffect } from 'react';
 
 // Fix default icon issue
-delete (L.Icon.Default.prototype as any)._getIconUrl;
+delete (L.Icon.Default.prototype as unknown as { _getIconUrl: () => void })._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
   iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
